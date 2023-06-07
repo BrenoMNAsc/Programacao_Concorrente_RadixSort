913b4d65256bcadbfd529bffae0532db7870a774
Danilo-Danilo-Danilo <danilo.lacerda@aluno.ufca.edu.br>
2023-06-06 21:37:04 -0300

    :zap: tá aí

--- RdxSortOpenMP.c
+++ RdxSortOpenMP.c
@@ -1,43 +1,45 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "omp.h"
+long int getMaxB(long int* a, long int n) {
+    long int max = a[0];
+    for (long int i = 1; i < n; i++) {
+        if (a[i] > max)
+            max = a[i];
+    }
+    return max;
+}
 
-#define BASE_BITS 8
-#define BASE (1 << BASE_BITS)
-#define MASK (BASE-1)
-#define DIGITS(v, shift) (((v) >> shift) & MASK)
 
-void RadixSortOpenMP(size_t n, long int data[n]) {
+void RadixSortOpenMPB(long int n, long int data[n]) {
+    long int max = getMaxB(data, n);
     long int * buffer = malloc(n*sizeof(long int));
-    int total_digits = sizeof(long int)*8;
+    long int i;
+    for (long int place = 1; max / place > 0; place *= 10) {
+        long int bucket[10] = {0};
 
-    //Each thread use local_bucket to move data
-    size_t i;
-    for(int shift = 0; shift < total_digits; shift+=BASE_BITS) {
-        size_t bucket[BASE] = {0};
-
-        size_t local_bucket[BASE] = {0}; // size needed in each bucket/thread
+        long int local_bucket[10] = {0}; // size needed in each bucket/thread
         //1st pass, scan whole and check the count
 #pragma omp parallel firstprivate(local_bucket)
         {
 #pragma omp for schedule(static) nowait
             for(i = 0; i < n; i++){
-                local_bucket[DIGITS(data[i], shift)]++;
+                local_bucket[(data[i] / place) % 10]++;
             }
 #pragma omp critical
-            for(i = 0; i < BASE; i++) {
+            for(i = 0; i < 10; i++) {
                 bucket[i] += local_bucket[i];
             }
 #pragma omp barrier
 #pragma omp single
-            for (i = 1; i < BASE; i++) {
+            for (i = 1; i < 10; i++) {
                 bucket[i] += bucket[i - 1];
             }
             int nthreads = omp_get_num_threads();
             int tid = omp_get_thread_num();
             for(int cur_t = nthreads - 1; cur_t >= 0; cur_t--) {
                 if(cur_t == tid) {
-                    for(i = 0; i < BASE; i++) {
+                    for(i = 0; i < 10; i++) {
                         bucket[i] -= local_bucket[i];
                         local_bucket[i] = bucket[i];
                     }
@@ -48,7 +50,7 @@ void RadixSortOpenMP(size_t n, long int data[n]) {
             }
 #pragma omp for schedule(static)
             for(i = 0; i < n; i++) { //note here the end condition
-                buffer[local_bucket[DIGITS(data[i], shift)]++] = data[i];
+                buffer[local_bucket[(data[i] / place) % 10]++] = data[i];
             }
         }
         //now move data
